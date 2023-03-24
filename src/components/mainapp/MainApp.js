081260dd97785ea9bf0c5d6f93f7513985d25134
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-03-24 13:24:29 +0530

    Show Profile picture, Logout

--- src/components/mainapp/MainApp.js
+++ src/components/mainapp/MainApp.js
@@ -1,17 +1,18 @@
 import React from "react"
-import { ProfileContext } from "../../context/ProfileContext"
+import Logout from "./logout/Logout"
 
 export default function MainApp() {
-  const profile = React.useContext(ProfileContext)
-  console.log(profile)
   return (
     <div className="grid-container">
-      <div className="grid-item item-logout">Logout</div>
-      <div className="grid-item item-session">Session</div>
-      <div className="grid-item item-search">Search</div>
-      <div className="grid-item item-calendar">Calendar</div>
-      <div className="grid-item item-today-panel">Today</div>
-      <div className="grid-item item-backlog-panel">Backlog</div>
+      <div className="grid-item item-logout">
+        <Logout />
+      </div>
+
+      <div className="grid-item item-session">Session 5</div>
+      <div className="grid-item item-search">Search 6</div>
+      <div className="grid-item item-calendar">Calendar 2</div>
+      <div className="grid-item item-today-panel">Today 3</div>
+      <div className="grid-item item-backlog-panel">Backlog 4</div>
     </div>
   )
 }
---
+++ src/components/mainapp/logout/Logout.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { ProfileContext } from "../../../context/ProfileContext"
+import { UserContext } from "../../../context/UserContext"
+import ExitIcon from "../../../assets/images/exit-icon.png"
+
+export default function Logout() {
+  const profile = React.useContext(ProfileContext)
+  const user = React.useContext(UserContext)
+
+  return (
+    <>
+      {profile && (
+        <>
+          <img
+            className="logout-logout-icon"
+            src={ExitIcon}
+            alt="exit"
+            onClick={() => {
+              if (window.confirm("You will be logged out!")) user.logout()
+            }}
+          />
+
+          <img src={profile.picture} referrerPolicy="no-referrer" />
+
+          <span title={profile.email} className="logout-profile-name">
+            {profile.given_name}
+          </span>
+        </>
+      )}
+    </>
+  )
+}
