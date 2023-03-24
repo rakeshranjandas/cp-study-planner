2e132a444269a282a64dcb1d69e5acde291af324
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-03-24 11:45:02 +0530

    MainApp Layout

--- src/components/mainapp/MainApp.js
+++ src/components/mainapp/MainApp.js
@@ -3,9 +3,15 @@ import { ProfileContext } from "../../context/ProfileContext"
 
 export default function MainApp() {
   const profile = React.useContext(ProfileContext)
+  console.log(profile)
   return (
-    <div>
-      <pre>{JSON.stringify(profile)}</pre>
+    <div className="grid-container">
+      <div className="grid-item item-logout">Logout</div>
+      <div className="grid-item item-session">Session</div>
+      <div className="grid-item item-search">Search</div>
+      <div className="grid-item item-calendar">Calendar</div>
+      <div className="grid-item item-today-panel">Today</div>
+      <div className="grid-item item-backlog-panel">Backlog</div>
     </div>
   )
 }
