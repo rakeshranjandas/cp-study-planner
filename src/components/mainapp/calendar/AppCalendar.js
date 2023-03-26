8a76f83979258bf62338656c5ef7c13026d90818
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-03-26 08:30:27 +0530

    Installed FullCalendar

--- src/components/mainapp/calendar/AppCalendar.js
+++ src/components/mainapp/calendar/AppCalendar.js
@@ -1,5 +1,90 @@
 import React from "react"
+import { formatDate } from "@fullcalendar/core"
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import timeGridPlugin from "@fullcalendar/timegrid"
+import interactionPlugin from "@fullcalendar/interaction"
+
+let eventGuid = 0
+function createEventId() {
+  return String(eventGuid++)
+}
 
 export default function AppCalendar() {
-  return <div>AppCalendar</div>
+  const [state, setState] = React.useState({
+    currentEvents: [],
+  })
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event")
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      })
+    }
+  }
+
+  const renderEventContent = (eventInfo) => {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
+  }
+
+  const handleEventClick = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove()
+    }
+  }
+
+  const handleEvents = (events) => {
+    setState({
+      currentEvents: events,
+    })
+  }
+
+  return (
+    <div className="app-calendar-div">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        initialView="dayGridMonth"
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        weekends={true}
+        //   initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+        select={handleDateSelect}
+        eventContent={renderEventContent} // custom render function
+        eventClick={handleEventClick}
+        eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+        /* you can update a remote database when these fire:
+      eventAdd={function(){}}
+      eventChange={function(){}}
+      eventRemove={function(){}}
+      */
+
+        height="100%"
+      />
+    </div>
+  )
 }
