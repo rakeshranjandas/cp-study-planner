bdd39a0ca6cf7cbe18c15987bc45e8639f749f87
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-03-27 05:06:11 +0530

    Initialise Calendar Service, Inject Context and Services to Calendar Machine

--- src/components/mainapp/calendar/Calendar.js
+++ src/components/mainapp/calendar/Calendar.js
@@ -5,15 +5,50 @@ import AppCalendar from "./AppCalendar"
 import CalendarLoader from "./CalendarLoader"
 import { calendarMachine } from "./calendarMachine"
 import { GoogleCalendarAPI } from "../../../services/google/GoogleCalendarAPI"
+import { CalendarService } from "../../../services/calendar/CalendarService"
 
-export default function Calendar() {
+export default function Calendar(props) {
   const user = React.useContext(UserContext)
+
+  const calendarService = React.useMemo(
+    () =>
+      new CalendarService({
+        panelsUpdater: props.panelsUpdater,
+        googleCalendarAPIInstance: new GoogleCalendarAPI(
+          user.user.access_token
+        ),
+        setAppCalendarEvents: props.setAppCalendarEvents,
+      }),
+    []
+  )
 
   const [state, send] = useMachine(calendarMachine, {
     context: {
-      googleCalendarApi: new GoogleCalendarAPI(user.user.access_token),
+      calendarService: calendarService,
+    },
+
+    services: {
+      listGoogleCalendars: async (context) =>
+        context.calendarService.listGoogleCalendars(),
+
+      checkIfGoogleCalendarPresent: async (context, event) => {
+        return context.calendarService.findAndSetGoogleCalendarId(event.data)
+      },
+
+      getGoogleCalendarEvents: async (context) => {
+        return context.calendarService.getGoogleCalendarEvents()
+      },
+
+      createGoogleCalendar: async (context) => {
+        await context.calendarService.createGoogleCalendar()
+      },
+
+      loadAppCalendarEvents: async (context) => {
+        await context.calendarService.loadEvents()
+      },
     },
   })
+
   return (
     <>
       {state.matches("ViewAppCalendar.Show") ? (
