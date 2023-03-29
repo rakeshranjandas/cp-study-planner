612c4954fe5efa5ef18b5f5b4268133ed63fdb18
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-03-29 09:20:23 +0530

    Convert timezone of google events

--- src/services/calendar/EventConvertor.js
+++ src/services/calendar/EventConvertor.js
@@ -3,12 +3,28 @@ export function googleCalendarEventToAppCalendarEvent(googleCalendarEvent) {
   return {
     id: googleCalendarEvent.id,
     title: googleCalendarEvent.summary,
-    start: googleCalendarEvent.start.date,
-    end: googleCalendarEvent.end.date,
+    start:
+      googleCalendarEvent.start.date ??
+      dateConvertTimezone(
+        googleCalendarEvent.start.dateTime,
+        googleCalendarEvent.start.timeZone
+      ),
+    end:
+      googleCalendarEvent.end.date ??
+      dateConvertTimezone(
+        googleCalendarEvent.end.dateTime,
+        googleCalendarEvent.end.timeZone
+      ),
     googleCalendarEvent: googleCalendarEvent,
   }
 }
 
 export function appCalendarEventToGoogleCalendarEvent(appCalendarEvent) {
   return {}
+}
+
+function dateConvertTimezone(dateStr, toTimezoneStr) {
+  return new Date(
+    new Date(dateStr).toLocaleString("en-US", { timeZone: toTimezoneStr })
+  ).toISOString()
 }
