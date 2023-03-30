3c9dcc858c81bc82cc98fbd69c0ceebabbd6236c
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-03-31 01:54:09 +0530

    Process description and properties

--- src/services/calendar/EventConvertor.js
+++ src/services/calendar/EventConvertor.js
@@ -1,11 +1,25 @@
 export function googleCalendarEventToAppCalendarEvent(googleCalendarEvent) {
   console.log(googleCalendarEvent, "googleCalendarEvent")
+
+  const descriptionRaw = googleCalendarEvent.description
+  const { description, properties } = {
+    ...Util.processDescriptionAndProperties(descriptionRaw),
+  }
+
   return {
     id: googleCalendarEvent.id,
     title: googleCalendarEvent.summary,
     start: googleCalendarEvent.start.date ?? googleCalendarEvent.start.dateTime,
     end: googleCalendarEvent.end.date ?? googleCalendarEvent.end.dateTime,
     allDay: !!googleCalendarEvent.start.date,
+
+    ...(description && {
+      description: description,
+    }),
+
+    ...(properties && {
+      properties: properties,
+    }),
 
     googleCalendarEvent: googleCalendarEvent,
   }
@@ -30,3 +44,37 @@ export function appCalendarEventToGoogleCalendarEvent(appCalendarEvent) {
     summary: appCalendarEvent.title,
   }
 }
+
+const Util = {
+  regexp: /(.*)(#cp-study-calendar#(.*)#\/cp-study-calendar#)(.*)/gs,
+
+  processDescriptionAndProperties: function (descRaw) {
+    if (!descRaw) return false
+
+    const matches = [...descRaw.matchAll(this.regexp)]
+
+    if (matches.length === 0 || matches[0].length < 5) return false
+
+    const data = matches[0][3]
+    if (!this._isJsonString(data)) return false
+
+    const ldata = matches[0][1]
+    const rdata = matches[0][4]
+
+    return {
+      description: ldata + rdata,
+      properties: JSON.parse(data),
+    }
+  },
+
+  _isJsonString(str) {
+    try {
+      JSON.parse(str)
+    } catch (e) {
+      return false
+    }
+    return true
+  },
+}
+
+// '#cp-study-calendar#{"tags":["dp","greedy"],"done":true}#/cp-study-calendar#'
