34f8634b65abb26744678ea1929aaccfc5dadacb
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-04-13 11:47:07 +0530

    Redirect Uri comments

--- src/constants/GoogleConstants.js
+++ src/constants/GoogleConstants.js
@@ -2,6 +2,8 @@ export const GoogleConstants = {
   CLIENT_ID:
     "995729197369-664mpelo0hrubg2t7k2vudsrg1i7evpf.apps.googleusercontent.com",
   CLIENT_SECRET: "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",
+
+  /* Google Cloud Console > Credentials > Authorised Javascript Origins & Authorised redirect URIs */
   // REDIRECT_URI: "http://localhost:3000",
   REDIRECT_URI: "https://rakeshranjandas.github.io",
 }
