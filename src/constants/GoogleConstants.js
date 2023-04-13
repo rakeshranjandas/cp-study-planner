1173e32f17df24aa6a37c776dd54754c1270cc61
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-04-13 11:02:02 +0530

    Attempt - fixing Redirect Uri

--- src/constants/GoogleConstants.js
+++ src/constants/GoogleConstants.js
@@ -3,5 +3,5 @@ export const GoogleConstants = {
     "995729197369-664mpelo0hrubg2t7k2vudsrg1i7evpf.apps.googleusercontent.com",
   CLIENT_SECRET: "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",
   // REDIRECT_URI: "http://localhost:3000",
-  REDIRECT_URI: "https://rakeshranjandas.github.io/cp-study-planner/",
+  REDIRECT_URI: "https://rakeshranjandas.github.io",
 }
