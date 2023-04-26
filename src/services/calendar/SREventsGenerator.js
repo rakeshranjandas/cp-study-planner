3e022b54438878c8e4a8fdcacbc7acc4c1ae5899
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-04-26 23:45:37 +0530

    SR Events Generator login

--- src/services/calendar/SREventsGenerator.js
+++ src/services/calendar/SREventsGenerator.js
@@ -1,5 +1,42 @@
 export const SREventsGenerator = {
-  generate: (appEvent) => {
-    return [appEvent]
+  generate: function (appEvent) {
+    if (!appEvent.formSR) return [appEvent]
+
+    const schemeList = appEvent.formSR.scheme
+    const schemeArr = schemeList.split(",").map((val) => parseInt(val))
+
+    const srId = new Date().getTime().toString()
+
+    const { formSR, ...firstAppEvent } = appEvent
+    firstAppEvent.properties.sr = {
+      scheme: schemeList,
+      id: srId,
+      day: schemeArr[0],
+    }
+
+    const appEventsList = [firstAppEvent]
+
+    for (let i = 1; i < schemeArr.length; i++) {
+      const daysDiff = schemeArr[i] - schemeArr[0]
+      const nAppEvent = structuredClone(firstAppEvent)
+      nAppEvent.start = this._addDays(firstAppEvent.start, daysDiff)
+      nAppEvent.end = this._addDays(firstAppEvent.end, daysDiff)
+      nAppEvent.properties.sr.day = schemeArr[i]
+
+      appEventsList.push(nAppEvent)
+    }
+
+    return appEventsList
+  },
+
+  _addDays: function (dateTime, daysDiff) {
+    const origSplitted = dateTime.split("T")
+    const dateStr = origSplitted[0]
+    const dateAfterAdd = new Date(
+      new Date(dateStr).getTime() + daysDiff * 24 * 60 * 60 * 1000
+    )
+    const dateStrAfterAdd = dateAfterAdd.toISOString().split("T")[0]
+
+    return dateStrAfterAdd + "T" + origSplitted[1]
   },
 }
